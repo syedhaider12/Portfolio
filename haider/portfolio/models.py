40c401925a29bd1d40173640e024e43886c5be9d
haider <hsyed5649@gmail.com>
2021-03-25 04:25:32 +0500

    add some css

--- haider/portfolio/models.py
+++ haider/portfolio/models.py
@@ -6,3 +6,4 @@ class contact(models.Model):
     emial = models.EmailField(max_length = 254)
     subject = models.CharField(max_length=50)
     textarea = models.TextField()
+
--- haider/portfolio/views.py
+++ haider/portfolio/views.py
@@ -4,13 +4,13 @@ from .models import contact
 
 
 def home (request):
-    
-    return render(request,'home.html', {'home': 'active'})
+    context = {'home' : 'active'}
+    return render(request,'home.html', context)
 def skills(request):
-     context = {'skills':'active'}
-     return render(request,'skills.html',context)
+    context = {'skills':'active'}
+    return render(request,'skills.html',context)
 def contac(request):
-    context = {'contact':'active',}
+    context = {'contact':'active'}
     if request.method =='POST':
         name = request.POST['fname']
         emial = request.POST['fmail']
@@ -18,9 +18,9 @@ def contac(request):
         textarea = request.POST['mess']
         obj = contact(name=name,emial=emial,subject=subject,textarea=textarea)
         obj.save();
-        return redirect('/contact')
+        return redirect('/contact',context)
     else:
         return render(request, 'contact.html',context)
 def service(request):
-     context = {'service':'active'}
-     return render(request, 'service.html',context)
+    context = {'service': 'active'}
+    return render(request, 'service.html',context)
